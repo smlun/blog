1d0a1bda82338cee66ad6ba2d10e384cd9bed7f5
Unknown <smlun@msn.com>
2017-02-07 14:03:28 +0800

    Declare posts resource

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  #declare standard REST resource
+  resources :posts
+
   #set the homepage to index.html
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
