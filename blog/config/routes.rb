fd4aae796fd72c84b3a6d006c82b01673e1759d2
Unknown <smlun@msn.com>
2017-02-07 15:59:00 +0800

    Fix showing of all posts

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   #declare standard REST resource
   resources :posts
 
-  #set the homepage to index.html
-  root 'welcome#index'
+  #set the homepage to post
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
