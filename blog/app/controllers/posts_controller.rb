cb4d726d6549145e8b4957481734bfb0506a57f9
Unknown <smlun@msn.com>
2017-02-08 10:24:28 +0800

    Fix partial forms

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def udpate
+  def edit
     @post = Post.find(params[:id])
   end
 
@@ -22,6 +22,16 @@ class PostsController < ApplicationController
       redirect_to @post
     else
       render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
