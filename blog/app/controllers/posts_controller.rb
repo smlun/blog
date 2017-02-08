03dfb903ab3e29d0a4808c771a407de445b8d9ca
Unknown <smlun@msn.com>
2017-02-08 12:22:42 +0800

    Fix CSS issues

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -35,6 +35,14 @@ class PostsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+   
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
