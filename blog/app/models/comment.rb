6dce97cf405a8d576f1952854e785beca16ba021
Unknown <smlun@msn.com>
2017-02-09 17:14:23 +0800

    Update comments validation requirements

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :content, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
+                    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
 end
