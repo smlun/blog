235d6fe57a1370ab59790ed51e8cb477d12ef480
Unknown <smlun@msn.com>
2017-02-09 13:35:50 +0800

    Insert Comment controller

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208064001) do
+ActiveRecord::Schema.define(version: 20170209052124) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
