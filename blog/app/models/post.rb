0a9ee588d3a0056b28991b8c4f1f957cab3d4e10
Unknown <smlun@msn.com>
2017-02-09 14:39:22 +0800

    Generate Comments controller

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
-  # validates :title, presence: true,
-  #                   length: { minimum: 15, maximum: 75 },
-  #                   uniqueness: { case_sensitive: false }
-  # validates :author, presence: true,
-  #                    length: { minimum: 5 },
-  #                    format: { with: /[a-z]/ }
-  # validates :content, presence: true,
-  #                     length: { in: 250..2000 }
   has_many :comments
+
+  validates :title, presence: true,
+                    length: { minimum: 15, maximum: 75 },
+                    uniqueness: { case_sensitive: false }
+  validates :author, presence: true,
+                     length: { minimum: 5 },
+                     format: { with: /[a-z]/ }
+  validates :content, presence: true,
+                      length: { in: 250..2000 }
 end
